3e1205aae2c2d91398ab49acdb50bbc53bbfb7a6
mistayo <96427953+mistayo@users.noreply.github.com>
2025-03-27 03:11:56 +0200

    Update ThemeProvider.tsx

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -27,7 +27,7 @@ export async function ThemeProvider({ children }: { children: ReactNode }) {
           Added `forcedTheme` to prevent the dark theme from being applied.
           TODO: remove `forcedTheme` when dark mode will be enabled
         */
-    <NextThemesThemeProvider attribute="class" forcedTheme="light">
+    <NextThemesThemeProvider attribute="class" forcedTheme="dark">
       <Theme accentColor={accentColor} hasBackground={false}>
         {children}
       </Theme>
